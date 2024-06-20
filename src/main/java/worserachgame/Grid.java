1fdc9cec4f1960e042ed810458f5a9f46a6a2f7d
Saitej Varri <saitejvarri@Saitejs-Mac-mini.local>
2024-06-20 20:02:07 +0530

    updated Grid

--- src/main/java/worserachgame/Grid.java
+++ src/main/java/worserachgame/Grid.java
@@ -1,7 +1,9 @@
 package worserachgame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,15 +11,21 @@ public class Grid {
 	
 	private int GRID_SIZE;
 	private char[][] contents;
-	private List<Corordinate> coordinates = new ArrayList<>();
+	private List<Coordinate> coordinates = new ArrayList<>();
 	
-	private class Corordinate {
+	private class Coordinate {
 		int x;
 		int y;
-		public Corordinate(int aX, int aY) {
+		public Coordinate(int aX, int aY) {
 			this.x = aX;
 			this.y = aY;
 		}
+	}
+	
+	private enum Direction {
+		HORIZONTAL,
+		VERTICAL,
+		DIAGONAL
 	}
 	
 	public Grid(int gridSize) {
@@ -27,28 +35,84 @@ public class Grid {
 		for (int i = 0; i < contents.length; i++) {
 			for (int j = 0; j < contents.length; j++) {
 				contents[i][j] = '_';
-				coordinates.add(new Corordinate(i, j));
+				coordinates.add(new Coordinate(i, j));
 			}
 			
 		}
 	}
 	
 	public void fillGrid(List<String> words) {
+		Collections.shuffle(coordinates);
 		for (String word : words) {
-			Collections.shuffle(coordinates);
 			
-			for(Corordinate corordinate :  coordinates) {
-				int x =  corordinate.x;
-				int y =  corordinate.y;
+			for(Coordinate coordinate :  coordinates) {
+				int x =  coordinate.x;
+				int y =  coordinate.y;
 				
-				if(y + word.length() < GRID_SIZE) {
-					for (char aChar : word.toCharArray()) {
-						contents[x][y++] =  aChar;
+				Direction selectedDirection =  getDirectionForFit(word, coordinate);
+				
+				if(selectedDirection != null) {
+					switch (selectedDirection) {
+					case HORIZONTAL:
+						for (char aChar : word.toCharArray()) {
+							contents[x][y++] =  aChar;
+						}
+						break;
+					case VERTICAL:
+						for (char aChar : word.toCharArray()) {
+							contents[x++][y] =  aChar;
+						}
+						break;
+					case DIAGONAL :
+						for (char aChar : word.toCharArray()) {
+							contents[x++][y++] =  aChar;
+						}
+						break;
 					}
+					break;
 				}
-				break;
 			}
 		}
+	}
+
+	private Direction getDirectionForFit(String word, Coordinate coordinate) {
+		List<Direction> dircetions =  Arrays.asList(Direction.values());
+		Collections.shuffle(dircetions);
+		
+		for (Direction direction : dircetions) {
+			if(doesFit(word, coordinate, direction)) {
+				return direction;
+			}
+		}
+		return null;
+	}
+
+	private boolean doesFit(String word, Coordinate coordinate, Direction direction) {
+		
+		int wordLength = word.length();
+		
+		switch (direction) {
+		case HORIZONTAL:
+			if (coordinate.y + wordLength > GRID_SIZE) return false;
+			for (int i = 0; i < word.length(); i++) {
+				if(contents[coordinate.x][coordinate.y + i] != '_') return false;
+			}
+			break;
+		case VERTICAL:
+			if (coordinate.x + wordLength > GRID_SIZE) return false;
+			for (int i = 0; i < word.length(); i++) {
+				if(contents[coordinate.x + i][coordinate.y] != '_') return false;
+			}
+			break;
+		case DIAGONAL:
+			if (coordinate.x + wordLength > GRID_SIZE 
+					|| coordinate.y + wordLength > GRID_SIZE) return false;
+			for (int i = 0; i < word.length(); i++) {
+				if(contents[coordinate.x + i][coordinate.y + i] != '_') return false;
+			}
+			break;
+		}
+		return true;
 	}
 	
 	public void displayGrid() {
